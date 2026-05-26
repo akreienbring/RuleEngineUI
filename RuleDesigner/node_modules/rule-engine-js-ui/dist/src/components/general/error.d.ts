2dae6517df3cf430ab90850629775b117bcc9333
André Kreienbring <akreienbring@users.noreply.github.com>
2026-05-26 11:30:51 +0200

    Delete RuleDesigner/node_modules directory

--- RuleDesigner/node_modules/rule-engine-js-ui/dist/src/components/general/error.d.ts
+++
@@ -1,3 +0,0 @@
-import { FallbackProps } from 'react-error-boundary';
-export declare function CreateRuleErrorFallback({ error, resetErrorBoundary, }: FallbackProps): import("react/jsx-runtime").JSX.Element;
-export declare function SimpleRuleErrorFallback({ error, resetErrorBoundary, }: FallbackProps): import("react/jsx-runtime").JSX.Element;
