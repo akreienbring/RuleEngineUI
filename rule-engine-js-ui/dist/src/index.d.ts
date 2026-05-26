e95281f7633201f936cc7d361c7935b1b9921f88
André Kreienbring <akreienbring@users.noreply.github.com>
2026-05-26 11:32:00 +0200

    Delete rule-engine-js-ui/dist directory

--- rule-engine-js-ui/dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as RuleEngineJSUI } from './components/rule-engine-js-ui';
-export { default as ObjectList } from '../../../../../../../components/full/object-list';
-export { default as BorderBox } from '../../../../../../../components/full/border-box';
-export { default as SimpleRuleList } from '../../../../../../../components/simple/simple-rule-list';
